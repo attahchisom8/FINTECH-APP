0bc395e26f13d947e448cf5e6a89aee3a972fa39
attahchisom8 <attahchisom8@gmail.com>
2026-02-09 13:07:40 +0100

    securing my intech aop

--- fintech-backend/src/routes/user.routes.ts
+++ fintech-backend/src/routes/user.routes.ts
@@ -3,7 +3,7 @@ import prisma from "../config/prisma";
 
 const userRouter = Router();
 
-userRouter.post("/users/sign-up", async (req, res) => {
+userRouter.post("/test/users", async (req, res) => {
 	const user = await prisma.user.create({
 		data: req.body,
 	});
