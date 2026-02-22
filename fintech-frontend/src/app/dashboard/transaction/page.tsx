be623b89704fdeb2323cbef1b6026af52fb1ba12
attahchisom8 <attahchisom8@gmail.com>
2026-02-22 22:49:03 +0000

    Transaction page for userz

---
+++ fintech-frontend/src/app/dashboard/transaction/page.tsx
@@ -0,0 +1,74 @@
+"use client"
+
+import { useState } from "react"
+import api from "@/lib/api";
+import ProtectedRoute from "@/components/protectedRoutes";
+
+export default function TransactionPage() {
+	const [amount, setAmount] = useState("");
+	const [description, setDescription] = useState("");
+	const [category, setCategory] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState("");
+
+	const handleSubmit = async (e: React.SubmitEvent) => {
+		e.preventDefault();
+		setError("");
+
+		try {
+			setLoading(true);
+
+			const res = await api.post("/ai/transactions", {
+				amount: Number(amount),
+				description
+			});
+			setCategory(res.data.category);
+      console.log(category);
+			setLoading(false);
+		} catch (err: any) {
+			const errMsg = err.response?.data?.message || err.message || "Transaction Error";
+      setError(errMsg);
+		} finally {
+			setLoading(false);
+		}
+	}
+
+	return (
+    <ProtectedRoute>
+		  <div className="trans-page">
+        <h2 className="text-title">Transactions</h2>
+        <div className="box-err">
+          {error && <p className="text-red">{error}</p>}
+        </div>
+        <div className="input-section">
+          <form onSubmit={ handleSubmit }>
+            <input
+              type="number"
+              placeholder="amount"
+              value={ amount }
+              required
+              onChange={(e) => setAmount(e.target.value)}
+              className="inp-amount"
+            />
+            <input
+              type="text"
+              placeholder="description"
+              value={ description }
+              onChange={ (e) => setDescription(e.target.value) }
+              required
+              className="inp-description"
+            />
+            <button
+              type="submit"
+              className="submit-btn"
+              disabled={ loading }
+              >
+              { loading ? "Processing...": "Submit" }
+              </button>
+          </form>
+        </div>
+        { category && ( <p className="text-category">{ category }</p> )}
+		  </div>
+    </ProtectedRoute>
+	)
+}
