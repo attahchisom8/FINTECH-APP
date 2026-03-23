9ccb0f697d946f45e0d918ba5e44048f7845902c
attahchisom8 <attahchisom8@gmail.com>
2026-03-23 14:50:34 +0000

    addex a store

---
+++ fintech-frontend/src/store/forecastStore.ts
@@ -0,0 +1,39 @@
+import { create } from "zustand";
+import api from "@/lib/api";
+
+interface ForecastDataShape {
+  predictedExpenses: number;
+  predictedBalance: number;
+  spendingTrend: string;
+  charData: {month: string, balance: number}[];
+}
+
+interface ForecastShape {
+  loading: boolean;
+  data: ForecastDataShape | null;
+  error: string;
+  fetchForcast: () => Promise<void>;
+}
+
+export const useForcastStore = create<ForecastShape>((set, get) => ({
+  loading: false,
+  data: null,
+  error: "",
+  fetchForcast: async () => {
+    if (get().data)
+      return;
+    if (get().loading)
+      return;
+
+    try {
+      set({loading: true, error: ""});
+      const res = await api.get("/forecast");
+      set({
+        data: res.data.forecast,
+        loading: false
+      });
+    } catch (err: any) {
+      set({ error: err.response?.data?.message || "fwtchihg forecast failed" });
+    }
+  }
+}))
